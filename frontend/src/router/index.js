f60777722b53ea4e5cc13d6bd9bde5c37bc48226
Hector <hasext.corptesla@gmail.com>
2026-05-08 16:37:46 -0500

    Vercel auto build with hash routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import Dashboard from '../views/Dashboard.vue'
@@ -25,7 +25,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
